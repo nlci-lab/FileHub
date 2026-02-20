189af1525ddf6c936abc501c12220fc45554fb05
Martin T Mathew <martintheodorus4@gmail.com>
2026-02-20 05:52:29 +0530

    placeholder

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+
+def main():
+    print("Hello, World!")
+    print("This is a sample Python program.")
+    print("Current working directory:", os.getcwd())    
+
+if __name__ == "__main__":
+    main()
